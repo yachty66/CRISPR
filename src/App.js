f48969c4f906365d8ab82196df6296ca44c9361d
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-01-13 11:15:04 +0100

    daily

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import { Products, Cart, Checkout, Toggle } from './components';
 import { commerce } from './lib/commerce';
 
@@ -133,7 +132,7 @@ const App = () => {
             <img src={'/assets/microscope.png'} alt="Logo" class="landing_page_image_2" />
             <div class="container_middle_page_up">
               <div class="container_middle_page_text">
-                The class has 1 lecture and experiment. You will use CRISPR to manipulate the phenotype of the flower daisy. This text can be longer and filled with more information "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+                <a>The class has 1 lecture and experiment. You will use CRISPR to manipulate the phenotype of the flower daisy. This text can be longer and filled with more information "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</a>
               </div>
             </div>
             <div class="container_middle_page_down">
@@ -144,16 +143,10 @@ const App = () => {
                 <div class="container_middle_page_text_small"><img class="container_middle_page_icon" src={"/assets/level.png"}/>Level<br/>Beginner</div>
                   <div></div>
                   <div></div>
-
                 </div>
-
               </div>
-
             </div>
           </div>
-          
-          
-          
           </>
 
 

4f0012287bca17e0c42c118d29f005935994c022
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-01-14 09:04:39 +0100

    d

--- src/test.js
+++ src/test.js
@@ -1,16 +1,62 @@
-{showCheckout && (
-    <div style={{ display: 'flex' }}>
-        <Route exact path="/">
-            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
-        </Route>
-        <CssBaseline />
-        <Switch>
-            <Route exact path="/cart">
-            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
-            </Route>
-            <Route path="/checkout" exact>
-            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
-            </Route>
-        </Switch>
-    </div>) 
+@media screen and (max-width: 520px) {
+    .container_end_page{
+        width: 100%;
+
+    }
+
+    .container_end_page_left{
+        margin-top: 10%;
+
+        width: 100%;
+        background: url("/assets/bacteria.png") no-repeat;
+        background-size:auto 120%;
+        background-position: center center;
+        padding-top: 3%;
+        align-items: center;
+        float: left;
+        display: flex;
+
+    }
+    
+    .container_end_page_right{
+        width: 0%;
+    }
+
+    .container_end_page_text_icon{
+        width: 20%;
+        float: right;
+    }
+    
+    .container_end_page_text_text{
+        font-family: 'Ubuntu Mono', monospace;
+        float: right;
+        width: 50%;
+        margin-right: 20%;
+        font-size: 3.5vw;
+    }
+
+    .container_headline_end_page{
+        height: 10%;
+        font-family: 'Ubuntu Mono', monospace;
+        font-size: 6vw;
+        text-align: right;
+    }
+
+    .container_end_page_footer_text{
+        width: 100%;
+        height: 50%;
+        text-align: center;
+        font-family: 'Ubuntu Mono', monospace;
+        font-size: 1.5vw;
+    }
+
+    .container_end_page_footer{
+        padding-top: 10%;
+        height: 20%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    
+
 }
